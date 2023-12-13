9e4a38b500af16893fb341ac72d5153d2823893f
Syed Talha <syedt.in00@gmail.com>
2023-12-13 22:57:01 +0500

    firebase added to project

---
+++ app.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYCWEZvndIG98iJ1NXDxrzfgkG2vphGUc",
+  authDomain: "firstproject-cb470.firebaseapp.com",
+  projectId: "firstproject-cb470",
+  storageBucket: "firstproject-cb470.appspot.com",
+  messagingSenderId: "77241558648",
+  appId: "1:77241558648:web:ebb9ca71db10cbaaa92f8d"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
